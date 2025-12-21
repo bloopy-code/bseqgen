5b4fceab4b0d686efd04de9c9f0db6ed4dcc99fc
danmeo <hutchinsond94@gmail.com>
2025-12-21 17:14:07 +0000

    😳 removing an errant print statement...

--- src/bseqgen/base.py
+++ src/bseqgen/base.py
@@ -11,7 +11,7 @@ class Direction(StrEnum):
 class BinarySequence:
     def __init__(self, bits: Sequence[int | str] | str) -> None:
         self.bits: list[int] = self._validate_bits(bits)
-        print(self.bits)  # REMOVE ME!!!! 
+        print(self.bits) 
 
     @staticmethod
     def _validate_bits(input_bits: Sequence[int | str] | str) -> list[int]:

65c1c119a2fd9bc827bbcf1fe9b1c19b61e404f6
danmeo <hutchinsond94@gmail.com>
2025-12-23 10:37:21 +0000

    updated tests including stubs for future features.

--- tests/test_binary_sequence.py
+++ tests/test_binary_sequence.py
@@ -50,13 +50,24 @@ def test_create_sequence_strlist() -> None:
     assert test_sequence.length == 2
 
 
-def test_bit_string_success(test_seq) -> None:
-    assert test_seq.bit_string == "110"
+def test__str__(test_seq):
+    assert str(test_seq) == "110"
 
 
-def test_len_matches_length(test_seq) -> None:
-    assert len(test_seq) == 3
-    assert len(test_seq) == test_seq.length
+def test_str_long_seq() -> None:
+    bits = [1, 0] * 100
+    seq = BinarySequence(bits)
+    seq_str = str(seq)
+
+    assert seq_str.startswith(seq.bit_string[:32])
+    assert seq_str.endswith(seq.bit_string[-32:])
+
+
+def test_repr_contains_class_and_len(test_seq) -> None:
+    r = repr(test_seq)
+
+    assert "BinarySequence" in r
+    assert "length=3" in r
 
 
 def test_eq_same_bits() -> None:
@@ -76,22 +87,21 @@ def test_eq_other_type(test_seq) -> None:
     assert test_seq != "110"
 
 
-def test_str_long_seq() -> None:
-    bits = [1, 0] * 100
-    seq = BinarySequence(bits)
-    seq_str = str(seq)
-    print(seq_str)
-    print("..." in seq_str)
-
-    assert seq_str.startswith(seq.bit_string[:32])
-    assert seq_str.endswith(seq.bit_string[-32:])
+def test_len_matches_length(test_seq) -> None:
+    assert len(test_seq) == 3
+    assert len(test_seq) == test_seq.length
 
 
-def test_repr_contains_class_and_len(test_seq) -> None:
-    r = repr(test_seq)
+def test__iter__(test_seq) -> None:
+    assert [b for b in test_seq] == [1, 1, 0]
 
-    assert "BinarySequence" in r
-    assert "length=3" in r
+
+def test__getitem__(test_seq) -> None:
+    assert test_seq[1:].bits == (1, 0)
+
+
+def test_bit_string_success(test_seq) -> None:
+    assert test_seq.bit_string == "110"
 
 
 def test_as_bytes_len3(test_seq) -> None:
@@ -111,6 +121,26 @@ def test_signed(test_seq) -> None:
     assert test_seq.signed == (1, 1, -1)
 
 
+def test_ones(test_seq) -> None:
+    assert test_seq.ones == 2
+
+
+def test_zeros(test_seq) -> None:
+    assert test_seq.zeros == 1
+
+
+def test_balance(test_seq) -> None:
+    assert test_seq.balance == 0.667
+
+
+def test_entropy(test_seq) -> None:
+    pass
+
+
+def test_copy_bits(test_seq) -> None:
+    pass
+
+
 def test_to_length_truncate() -> None:
     bits = [1] * 100
     seq = BinarySequence(bits)
@@ -124,7 +154,41 @@ def test_to_length_truncate() -> None:
 def test_shift_left(test_seq) -> None:
     assert test_seq.shift(1, 'left').bits == (1, 0, 1)
     assert test_seq.shift(2).bits == (0, 1, 1)
+    assert test_seq.shift(-1).bits == (0, 1, 1)
 
 
 def test_shift_right(test_seq) -> None:
     assert test_seq.shift(1, 'right').bits == (0, 1, 1)
+    assert test_seq.shift(-1, 'right').bits == (1, 0, 1)
+
+
+def test_autocorr(test_seq) -> None:
+    pass
+
+
+def test_crosscor(test_seq) -> None:
+    pass
+
+
+def test_run_lengths(test_seq) -> None:
+    pass
+
+
+def test_to_numpy(test_seq) -> None:
+    pass
+
+
+def test_from_numpy(test_seq) -> None:
+    pass
+
+
+def test_invert(test_seq) -> None:
+    pass
+
+
+def test_xor(test_seq) -> None:
+    pass
+
+
+def test_hamming_distance(test_seq) -> None:
+    pass
